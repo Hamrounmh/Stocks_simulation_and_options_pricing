c2493ca15a239437498e4924b9fe27f75e304acc
Hamroun Houssem <hamrounhoussem@gmail.com>
2021-03-08 13:42:50 +0100

    simulation d'un actif avec le shéma d'euler

---
+++ TP1/Exo3.py
@@ -0,0 +1,19 @@
+import numpy as np
+import numpy.random as sim
+import matplotlib.pyplot as plt
+
+T=4
+n=10
+N=1 # nombre de trajectoirs
+pas =T/n
+
+B=np.zeros((n+1,N))
+#Bt0=0 déja initialisé avec la matrice B.zero
+
+for j in range(N):
+    for i in range(1,n+1):
+        B[i,j]=B[i-1,j]+np.sqrt(pas)*sim.randn()
+
+dates=np.linspace(0,T,n+1) # n+1 dates
+graph=plt.plot(dates,B)
+plt.show()
